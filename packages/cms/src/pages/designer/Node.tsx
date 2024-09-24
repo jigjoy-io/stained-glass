77a9765200911825550b279c7805f10b6ae42463
MilanJulinac42 <milanjulinac996@gmail.com>
2024-09-24 17:47:27 +0200

    page name is no longer being reset

--- packages/cms/src/pages/designer/Node.tsx
+++ packages/cms/src/pages/designer/Node.tsx
@@ -170,9 +170,11 @@ export function Node(props: any) {
         let result = JSON.parse(JSON.stringify(pages))
         let index = result.findIndex((page) => page.id == newPage.id)
         result.splice(index, 1, newPage)
-        dispatch(pagesUpdated(result))
-
-
+        if (newPage.id == activePage.id) {
+            dispatch(rootPageUpdated(newPage))
+            dispatch(pageUpdated(newPage))
+        }
+        
     }
 
     const expandDropdown = async (e: React.MouseEvent, id) => {
