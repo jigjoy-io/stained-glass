a5c30c28cecd6f261926c42853b8ed5c0d5501f3
MilanJulinac42 <milanjulinac996@gmail.com>
2024-09-26 16:34:54 +0200

    removed unused code

--- packages/cms/src/components/Page.tsx
+++ packages/cms/src/components/Page.tsx
@@ -1,34 +1,14 @@
-import React, { useState, useEffect } from "react"
+import React from "react"
 import { usePage } from "../util/store"
 import { PageFactory } from "../factories/PageFactory"
-import Loader from "../components/loader/Loader"
 
 function Page() {
+
   const page = usePage()
-  const [isLoading, setIsLoading] = useState(true)
 
-  useEffect(() => {
-    setIsLoading(true)
-    if (page != null) {
-      setIsLoading(false)
-    } else {
-      setIsLoading(true)
-    }
-  }, [page])
+  return <div className="grow flex flex-col justify-center items-center">{page != null && PageFactory.get(page)}</div>
 
-  if (isLoading) {
-    return (
-      <div className="grow flex flex-col justify-center items-center">
-        <Loader />
-      </div>
-    )
-  }
-
-  return (
-    <div className="grow flex flex-col justify-center items-center">
-      {PageFactory.get(page)}
-    </div>
-  )
 }
+
 
 export default Page
