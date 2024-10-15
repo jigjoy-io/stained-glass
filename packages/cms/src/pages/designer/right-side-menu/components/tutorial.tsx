00bc1607926fc8c9b26eb8d42e92dd3a05c2074e
Mijura <miodragvilotijevic@gmail.com>
2024-10-15 11:45:04 +0200

    integrated tutorial in the right side menu

---
+++ packages/cms/src/pages/designer/right-side-menu/components/tutorial.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default function Tutorial() {
+
+    const TUTORIAL_SRC = process.env.REACT_APP_JIGJOY_TUTORIAL
+
+    return <iframe src={TUTORIAL_SRC} className="grow w-full"/>
+}
--- packages/cms/src/pages/designer/right-side-menu/right-side-menu.tsx
+++ packages/cms/src/pages/designer/right-side-menu/right-side-menu.tsx
@@ -52,7 +52,7 @@ export function RightSideMenu() {
         <AnimatePresence>{
             sidebar.visible && <m.div variants={animation} initial="hidden" animate="show" exit="exit" transition={transition}>
 
-                <div className="border-l border-light shadow-lg fixed right-0 bottom-0 h-[100dvh] w-[30%] bg-[white]">
+                <div className="border-l border-light shadow-lg fixed right-0 bottom-0 h-[100dvh] w-[50%] bg-[white] flex flex-col">
 
                     <div className="h-[24px] w-[24px] m-2" onClick={() => dispatch(sidebarExpanded({ expanded: false, component: null }))}>
                         <ToolbarButtonWrapper tooltip={<div className="text-[14px]">{localization.closePanel}</div>}>
