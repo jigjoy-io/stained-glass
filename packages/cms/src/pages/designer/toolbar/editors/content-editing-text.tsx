a929a8d63af0f0e0cc0ddd3d00f24f736b88f551
MilanJulinac42 <milanjulinac996@gmail.com>
2024-10-25 16:30:18 +0200

    JIG 270 added basic backspace functionality

--- packages/cms/src/pages/designer/toolbar/editors/content-editing-text.tsx
+++ packages/cms/src/pages/designer/toolbar/editors/content-editing-text.tsx
@@ -1,17 +1,27 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { focusBlock, insertBlock, updateBlock } from '../../../../reducers/page-reducer'
-import { useActiveBlock } from '../../../../util/store'
+import { useDispatch, useSelector } from 'react-redux'
+import { focusBlock, insertBlock, updateBlock, removeBlock } from '../../../../reducers/page-reducer'
+import { useActiveBlock, usePage } from '../../../../util/store'
 import textEditingVariants from '../../../../util/style-helper/text-editing-variations'
 import alignmentVariations from '../../../../util/style-helper/alignment-variations'
 import TemplateFactory from '../../../../util/factories/templates/template-factory'
 import { splitTextAtCursor } from '../../../../util/cursor-helper/split-text-at-cursor'
 
 export default function ContentEditingText(props: any) {
-
 	const [position, setPosition] = useState(props.position)
 	const [type, setType] = useState(props.type)
 	const [style, setStyle] = useState({} as any)
+	const page = usePage()
+
+	const previousBlock = useSelector((state: any) => {
+		console.log("BLOCK", page.config.buildingBlocks)
+		const blocks = page.config.buildingBlocks
+		const currentIndex = blocks.findIndex((block: any) => block.id === props.id)
+		if (currentIndex > 0) {
+			return blocks[currentIndex - 1]
+		}
+		return null
+	})
 
 	useEffect(() => {
 		setPosition(props.position)
@@ -33,14 +43,12 @@ export default function ContentEditingText(props: any) {
 
 	const updateText = (event: any) => {
 		let newValue = event.target.innerText.trim()
-
 		let block = {
 			id: props.id,
 			position: props.position,
 			type: props.type,
 			text: newValue
 		}
-
 		dispatch(updateBlock(block))
 	}
 
@@ -51,10 +59,8 @@ export default function ContentEditingText(props: any) {
 			const preCaretRange = document.createRange()
 			preCaretRange.selectNodeContents(element)
 			preCaretRange.setEnd(range.endContainer, range.endOffset)
-
 			const contents = Array.from(element.childNodes)
 			let position = 0
-
 			for (let i = 0; i < contents.length; i++) {
 				const node = contents[i]
 				if (node === range.endContainer) {
@@ -66,32 +72,67 @@ export default function ContentEditingText(props: any) {
 					position += 1
 				}
 			}
-
 			return position
 		}
 		return 0
 	}
 
+	const handleMergeWithPreviousBlock = () => {
+		if (!previousBlock) return false
+
+		const currentText = ref.current?.innerText || ""
+		const prevBlockElement = document.querySelector(`[data-block-id="${previousBlock.id}"]`) as HTMLElement
+		if (!prevBlockElement) return false
+
+		const prevText = prevBlockElement.innerText
+		const mergedText = prevText + currentText
+
+		// Update previous block with merged text
+		dispatch(updateBlock({
+			...previousBlock,
+			text: mergedText
+		}))
+
+		// Remove current block
+		dispatch(removeBlock(props.id))
+
+		// Focus previous block and set cursor at merge point
+		dispatch(focusBlock(previousBlock.id))
+
+		// Use setTimeout to ensure DOM is updated before setting cursor
+		setTimeout(() => {
+			const updatedPrevBlock = document.querySelector(`[data-block-id="${previousBlock.id}"]`) as HTMLElement
+			if (updatedPrevBlock) {
+				const range = document.createRange()
+				const sel = window.getSelection()
+				const textNode = updatedPrevBlock.firstChild || updatedPrevBlock
+
+				// Set cursor position to where the texts were merged
+				range.setStart(textNode, prevText.length)
+				range.collapse(true)
+				sel?.removeAllRanges()
+				sel?.addRange(range)
+				updatedPrevBlock.focus()
+			}
+		}, 0)
+
+		return true
+	}
 
 	const handleKeyDown = (event: any) => {
 		if (event.key === 'Enter' && event.shiftKey) {
 			return
 		} else if (event.key === 'Enter') {
 			event.preventDefault()
-
 			if (!ref.current) return
-
 			const caretPosition = getCaretPosition(ref.current)
 			const text = ref.current?.innerText || ""
 			const isCaretAtEnd = caretPosition === ref.current?.innerText.length
-
 			const { beforeCursor, afterCursor } = splitTextAtCursor(text, caretPosition)
 
 			let newBlock = TemplateFactory.create(props.type)
-
 			if ((beforeCursor || afterCursor) && !isCaretAtEnd) {
 				ref.current!.innerText = beforeCursor
-
 				let updatedBlock = {
 					id: props.id,
 					position: props.position,
@@ -99,17 +140,13 @@ export default function ContentEditingText(props: any) {
 					text: beforeCursor
 				}
 				dispatch(updateBlock(updatedBlock))
-
 				newBlock.text = afterCursor
-
 				dispatch(insertBlock({
 					referenceBlock: props.id,
 					block: newBlock,
 					position: 'below'
 				}))
-
 				dispatch(focusBlock(newBlock.id))
-
 				ref.current?.blur()
 			} else if (isCaretAtEnd && !event.shiftKey) {
 				let selector = TemplateFactory.createBlockSelector()
@@ -119,6 +156,12 @@ export default function ContentEditingText(props: any) {
 					position: 'below'
 				}))
 				dispatch(focusBlock(selector.id))
+			}
+		} else if (event.key === 'Backspace') {
+			const caretPosition = getCaretPosition(ref.current!)
+			if (caretPosition === 0 && previousBlock) {
+				event.preventDefault()
+				handleMergeWithPreviousBlock()
 			}
 		}
 	}
