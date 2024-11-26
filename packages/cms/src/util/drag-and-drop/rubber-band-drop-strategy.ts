0497bf094bb96c22e57673328d2876002a778be9
MilanJulinac42 <milanjulinac996@gmail.com>
2024-11-26 12:35:28 +0100

    JIG 414 removed unused code and packages

--- packages/cms/src/util/drag-and-drop/rubber-band-drop-strategy.ts
+++ packages/cms/src/util/drag-and-drop/rubber-band-drop-strategy.ts
@@ -22,32 +22,20 @@ export class RubberBandDropStrategy implements DropStrategy {
 	execute(dropTarget, selectedBlocks, blocks, page, activeCarousel, dispatch, setDropTarget) {
 		if (!dropTarget || !selectedBlocks?.length) return
 
-		// Collect IDs of selected blocks
-		console.log("SLEC", selectedBlocks)
-		console.log("BLOX", blocks)
 		const selectedBlockIds = selectedBlocks.map((block) => block.id)
-		console.log("T1")
 
-		// Remove selected blocks from the current block list
 		const filteredBlocks = blocks.filter((block) => !selectedBlockIds.includes(block.id))
-		console.log("T2", dropTarget)
 
-		// Find the target block's index
 		const targetBlockIndex = blocks.findIndex((block) => block.id === dropTarget.block.id)
 		if (targetBlockIndex === -1) return
-		console.log("T3")
 
-		// Adjust target index based on drop position
 		const adjustedTargetIndex = dropTarget.position === "top" ? targetBlockIndex : targetBlockIndex + 1
 
-		// Insert selected blocks at the new position
 		filteredBlocks.splice(adjustedTargetIndex, 0, ...selectedBlocks)
 
-		// Find the target page
 		const carouselPage = findPageById(page, activeCarousel)
 		const targetPage = carouselPage || page
 
-		// Update the state with the rearranged blocks
 		dispatch(
 			updateBlock({
 				...targetPage,
@@ -57,8 +45,6 @@ export class RubberBandDropStrategy implements DropStrategy {
 				},
 			}),
 		)
-
-		// Reset the drop target
 		setDropTarget(null)
 	}
 }
