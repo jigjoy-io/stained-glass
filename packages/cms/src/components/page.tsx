2681767aa005ca2428f3cc4b568246ccc554463f
Mijura <miodragvilotijevic@gmail.com>
2024-12-17 15:08:23 +0100

    avoid fetching active page from redux in carousel

--- packages/cms/src/components/page.tsx
+++ packages/cms/src/components/page.tsx
@@ -1,11 +1,14 @@
-import React from "react"
-import { usePage } from "../util/store"
+import React, { useEffect, useState } from "react"
 import { PageFactory } from "../util/factories/page-factory"
 
-function Page() {
-	const page = usePage()
+function Page(props) {
+	const [page, setPage] = useState(props.page)
 
-	return <div className="flex flex-col h-full justify-center items-center">{page != null && PageFactory.get(page)}</div>
+	useEffect(() => {
+		setPage(props.page)
+	}, [props.page])
+
+	return <div className="flex flex-col h-full justify-center items-center">{page && PageFactory.get(page)}</div>
 }
 
 export default Page
